9813359e51cfb6ba2076969103c8b6464e2dd759
Alex Steiner <privat@alexsteiner.de>
2018-08-30 13:51:54 +0200

    Change run.sh script

--- run.sh
+++ run.sh
@@ -7,16 +7,17 @@ docker rm osmnames-sphinxsearch
 set -ex
 
 #SOURCE_DIR=/data/osmnames-sphinxsearch
-SOURCE_DIR=`pwd`
+#SOURCE_DIR=`pwd`
+SOURCE_DIR=~
 mkdir -p $SOURCE_DIR/data/input $SOURCE_DIR/data/index $SOURCE_DIR/tmp $SOURCE_DIR/log
 sudo rm -rf $SOURCE_DIR/tmp
 
-docker build -t klokantech/osmnames-sphinxsearch:devel .
+docker build -t alexsteinerde/osmnames-sphinxsearch:devel .
 docker run -d --name osmnames-sphinxsearch \
-    -p 9013:80 -p 9313:9312 \
+    -p 80:80 -p 9313:9312 \
     -v `pwd`/web/:/usr/local/src/websearch/ \
     -v $SOURCE_DIR/data/:/data/ \
     -v $SOURCE_DIR/tmp/:/tmp/ \
     -v $SOURCE_DIR/log/:/var/log/supervisord/ \
-    klokantech/osmnames-sphinxsearch:devel
+    alexsteinerde/osmnames-sphinxsearch:devel
 
